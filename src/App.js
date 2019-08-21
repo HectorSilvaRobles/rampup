59d3ac3bc7bc52717c3559e847e2fade3c49252a
HSR <hectorsilvarobles@gmail.com>
2019-08-21 11:21:06 -0700

    finished the foundation of layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header'
 import Hero from './Components/Hero/Hero'
+import UnderHero from './Components/UnderHero/UnderHero'
+import ProductIntro from './Components/ProductIntro/ProductIntro'
+import HowItWorks from './Components/HowItWorks/HowItWorks'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       </header>
       <body>
         <Hero />
+        <UnderHero />
+        <ProductIntro />
+        <HowItWorks />
+        <Footer />
       </body>
     </div>
   );
